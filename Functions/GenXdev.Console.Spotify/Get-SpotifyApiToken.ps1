cabf895b8250c6583f8fdb974be08e7c7f8a299f
René Vaessen <rene.vaessen@gmail.com>
2025-09-23 07:54:50 +0200

    Release 1.284.2025

--- Functions/GenXdev.Console.Spotify/Get-SpotifyApiToken.ps1
+++ Functions/GenXdev.Console.Spotify/Get-SpotifyApiToken.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.Console.Spotify
 Original cmdlet filename  : Get-SpotifyApiToken.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.280.2025
+Version                   : 1.284.2025
 ################################################################################
 MIT License
 
